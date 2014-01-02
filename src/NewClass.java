dc7fbee3e6987e11a1af43187413abdb8bcf15d6
Administrador <Administrador@UPS-SCL-021.utpl.edu.ec>
2014-01-02 11:14:02 -0500

    Iniciio de proyecto Condor

---
+++ src/NewClass.java
@@ -0,0 +1,12 @@
+
+import com.acolita.deberes.semestre4.BaseDatos;
+import com.acolita.deberes.semestre2.*;
+import com.acolita.deberes.semestre2.troncales.EstructuraDatos;
+import com.acolita.deberes.semestre1.Matematicas;
+
+public class NewClass {
+    public static void main(String[] args) {
+       Matematicas M = new Matematicas();
+        System.out.println(M.getNota());
+    }
+}
